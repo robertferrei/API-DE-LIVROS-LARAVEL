dc3987322e5e304c3c15646aa00aec23c89ed41f
robertferrei <ferreirarobert727@gmail.com>
2024-06-01 08:59:23 -0300

    manipulando routes e efetuando testes

---
+++ API-LIVROS-LARAVEL/resources/views/livros.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>testando</h1>
+ 
+
+    <a href="{{route('home-index')}}">clique nesse link</a>
+</body>
+</html>
--- API-LIVROS-LARAVEL/routes/web.php
+++ API-LIVROS-LARAVEL/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('welcome');
+
+//Route::view('/livros','livros');
+//Route::view('/livros','livros',['name' => 'SENHOR DOS ANEIS']);
+//parametros para busca 
+// Route::get('/livros/{name?}',function($name=null){
+//     return view('livros',['nomeLivros'=>$name]);
+// });
+
+
+//Route::get('/livros/{id?}/{name?}',function($id=null,$name=null){
+  //  return view('livros',['idLivros'=>$id,'nomeLivros'=>$name]);
+//})->where(['id','[0-9]+','name'=> '[a-z]+']);
+
+Route::get('/livros',function(){
+    return  view('livros');
 });
+
+ Route::get('/home', function () {
+     return view('welcome');
+ })->name('home-index');
+
+
+ //caso de erro de encontrar erro
+ Route::fallback(function(){
+    return "erro ao localizar a rota";
+ });
+
